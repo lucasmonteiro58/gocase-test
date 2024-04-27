960102464aad1b9114788b459ea49faef2f0df4a
Lucas Monteiro <lucas.ribeiro@drcuco.com.br>
2024-04-27 19:39:03 -0300

    feat: ✨Refactor useBackground.ts to add lockImage and unlockImage functions

--- composables/useBackground.ts
+++ composables/useBackground.ts
@@ -14,12 +14,13 @@ export function useBackground() {
           lockScalingAspectRatio: true,
           top: 300,
           index: -9999,
+          globalCompositeOperation: "destination-over",
+          lockUniScaling: true,
         });
         oImg.scaleToWidth((canvas.value?.width as number) - 50);
         backgroundImage.value = oImg;
 
         canvas.value.insertAt(markRaw(backgroundImage.value), 0, false);
-        //canvas.value.add(markRaw(backgroundImage.value));
       });
 
       canvas.value.renderAll();
@@ -36,5 +37,25 @@ export function useBackground() {
     }
   }
 
-  return { addImage, removeImage };
+  function lockImage() {
+    if (backgroundImage.value) {
+      backgroundImage.value.set({
+        hoverCursor: "default",
+        selectable: false,
+      });
+      canvas.value.discardActiveObject().renderAll();
+    }
+  }
+
+  function unlockImage() {
+    if (backgroundImage.value) {
+      backgroundImage.value.set({
+        hoverCursor: "move",
+        selectable: true,
+      });
+      canvas.value.setActiveObject(backgroundImage.value).renderAll();
+    }
+  }
+
+  return { addImage, removeImage, lockImage, unlockImage };
 }
