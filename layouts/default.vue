6c0c60b9a39a575c2abf0d380499c62481306bd4
Lucas Monteiro <lucas.ribeiro@drcuco.com.br>
2024-04-27 22:42:40 -0300

    style: 💎Add NuxtLoadingIndicator component to default.vue layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,7 @@
   <div
     class="tw-w-full tw-h-screen tw-flex tw-justify-center tw-flex-col tw-items-center tw-bg-primary-50"
   >
+    <NuxtLoadingIndicator color="#f59e0b" />
     <NuxtPage />
   </div>
 </template>
